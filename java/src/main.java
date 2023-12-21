0f6af245c6bfef85781e417c4bdd4e1548d60a73
lluckl <lordlucasrj@gmail.com>
2023-12-20 22:42:31 -0300

    finalizando o exercicio 1

---
+++ java/src/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+
+        objetoLampada n1 = new objetoLampada();
+        n1.Marca = "Philips";
+        n1.modelo = "LED";
+        n1.Cor = "Branco";
+        n1.Valor = 21.23f;
+        n1.volts = 110;
+        n1.lampada();
+
+    }
+}
